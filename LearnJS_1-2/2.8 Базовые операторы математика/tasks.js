1611e2e362231bacd3c453ab7946e3880c9945ad
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2023-12-05 14:51:40 +0300

    dfdf

--- LearnJS_1-2/2.8 Базовые операторы математика/tasks.js
+++ LearnJS_1-2/2.8 Базовые операторы математика/tasks.js
@@ -1,4 +1,4 @@
 console.log(10 + 10 + 10 + 10 + "1" + 50); // 40150 - string
 
 console.log("10" + "10"); // 1010 - string
-console.log(+"10" + +"10"); // 20 - number
+console.log(Number("10") + Number("10")); // 20 - number
