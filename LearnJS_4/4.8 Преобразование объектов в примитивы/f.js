b082403b8163c17f3f0b61043b8f52208e4ff407
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2023-12-27 17:36:58 +0300

    dfdf

---
+++ LearnJS_4/4.8 Преобразование объектов в примитивы/f.js
@@ -0,0 +1,34 @@
+const user = {
+  name: "Bob",
+  money: 10000,
+
+  [Symbol.toPrimitive](hint) {
+    console.log(`Хинт - ${hint}`);
+
+    return hint == "string" ? `{name: ${this.name}}` : this.money;
+  },
+};
+
+console.log(user);
+console.log(+user);
+console.log(user + 5000);
+
+const test = {
+  number: 50,
+  toString() {
+    return this.number;
+  },
+};
+
+console.log(test * 2);
+
+///////////
+
+const obj = {
+  name: "popa",
+  "my age": 15,
+};
+
+obj.isAdmin = true;
+
+console.log(obj["my age"]);
