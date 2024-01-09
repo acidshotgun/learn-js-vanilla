03b9ac5c1a3b31c6b4447994e23956d623fda242
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-01-09 19:51:04 +0300

    dfdf

---
+++ Задачи 6Seniors/Ферма/index.js
@@ -0,0 +1,154 @@
+class Customer {
+  order = {};
+
+  constructor(name, money) {
+    this.name = name;
+    this.money = money;
+  }
+
+  // Оплотить корзину
+  buyProducts(store, order = this.order) {}
+
+  // Добавить товар в корзину
+  addItemsToOrder(store, product, count) {
+    const item = store.warehouse.sendProductFromWarehouse(product, count);
+
+    if (item.status !== false) {
+      const { name, price, count } = item;
+
+      if (name in this.order) {
+        console.log(`Позиция "${product}" добавлена в корзину.`);
+        return (this.order[name].count += count);
+      }
+
+      console.log(`Позиция "${product}" ${count}шт. добавлена в корзину.`);
+      return (this.order[item.name] = { price, count });
+    } else {
+      return console.log(item.message);
+    }
+  }
+
+  // Удалить товар из корзины
+  removeItemsFromOrder(product, count = 0) {
+    if (product in this.order) {
+      if (count === 0) {
+        console.log(`Позиция "${product}" удалена.`);
+        return delete this.order[product];
+      }
+
+      console.log(`Позиция "${product}" ${count}шт. удалена.`);
+      return (this.order[product].count -= count);
+    } else {
+      console.log(`Позиции "${product}" нет в чеке.`);
+    }
+  }
+
+  // Посмотреть корзину
+  getOrder() {
+    let counter = 0;
+
+    for (let key in this.order) {
+      counter += this.order[key].price * this.order[key].count;
+
+      console.log(
+        `${key}: ${this.order[key].count}шт. на сумму ${
+          this.order[key].count * this.order[key].price
+        } руб.`
+      );
+    }
+
+    console.log(`Общая сумма ${counter} руб.`);
+  }
+}
+
+// Поставка на склад | начальное состояние склада
+const supply = [
+  { name: "Кроссовки", price: 400, count: 20 },
+  { name: "Футболка", price: 100, count: 40 },
+  { name: "Штаны", price: 250, count: 35 },
+  { name: "Носки", price: 50, count: 100 },
+];
+
+class Warehouse {
+  constructor(initialProduct) {
+    this.warehouseStock = initialProduct;
+  }
+
+  /*
+   Найти конкретный продукт со склада.
+    Покупатель выбирает товар,
+    Если товар есть на складе, то он получает объект с данными этого товара
+  */
+  sendProductFromWarehouse(product, count) {
+    for (let i = 0; i < this.warehouseStock.length; i++) {
+      if (
+        product === this.warehouseStock[i].name &&
+        count <= this.warehouseStock[i].count
+      ) {
+        // Не делаем, тк товар не продается, а просто добавляется в корзину
+        // this.warehouseStock[i].count -= count;
+
+        return {
+          name: this.warehouseStock[i].name,
+          price: this.warehouseStock[i].price,
+          count: count,
+        };
+      }
+    }
+
+    return {
+      status: false,
+      message: "Такого кол-ва товара нет на складе.",
+    };
+  }
+
+  // Проверить сток товаров на складе
+  checkWaehouseStock() {
+    return this.warehouseStock;
+  }
+
+  // Запрос поставки на склад.
+  getGoodsForStock(supply) {
+    for (let i = 0; i < supply.length; i++) {
+      let isFound = false;
+
+      for (let j = 0; j < this.warehouseStock.length; j++) {
+        if (supply[i].name === this.warehouseStock[j].name) {
+          this.warehouseStock[j].count += supply[i].count;
+
+          isFound = true;
+          break;
+        }
+      }
+
+      if (!isFound) {
+        this.warehouseStock.push(supply[i]);
+        isFound = false;
+      }
+    }
+  }
+}
+
+class Store {
+  constructor(name) {
+    this.name = name;
+    this.money = 0;
+    this.warehouse = new Warehouse(supply);
+  }
+
+  toSell(order) {}
+}
+
+const customerJack = new Customer("Jack", 1500);
+const adidas = new Store("Adidas");
+
+console.log(customerJack);
+// console.log(adidas.warehouse.sendProductFromWarehouse("Кроссовки", 3));
+customerJack.addItemsToOrder(adidas, "Кроссовки", 3);
+customerJack.addItemsToOrder(adidas, "Кроссовки", 3);
+customerJack.addItemsToOrder(adidas, "Футболка", 3);
+customerJack.addItemsToOrder(adidas, "Футболка", 50);
+customerJack.addItemsToOrder(adidas, "Носки", 2);
+customerJack.removeItemsFromOrder("Кроссовки", 2);
+customerJack.removeItemsFromOrder("Шорты", 2);
+customerJack.getOrder();
