ab1c2db10d1914afe371e9437e90e7f2cfe5bc75
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-02-08 13:28:43 +0300

    df

---
+++ LearnJS_5/5.4 Массивы/just.js
@@ -0,0 +1,7 @@
+const arr = ["привет", "как дела?", 10];
+
+// const arr2 = ["привет", "как дела?", 10]; // тут false (разные объекты)
+
+const arr2 = arr; // тут true (ссылка)
+
+console.log(arr == arr2); //
