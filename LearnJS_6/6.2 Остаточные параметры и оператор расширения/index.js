c81fa65974f04f15b42618d5f459eb8fd206a6ed
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-03-22 12:32:59 +0300

    dfdf

--- LearnJS_6/6.2 Остаточные параметры и оператор расширения/index.js
+++ LearnJS_6/6.2 Остаточные параметры и оператор расширения/index.js
@@ -4,3 +4,21 @@ console.log(...set); // Развернули Set
 
 let arrayFromSet = [...set]; // Можно развернуть set в новый массив
 console.log(arrayFromSet); // [ 1, 2, 3, 4, 'hello' ]
+
+///
+
+let obj = {
+  name: "sasha",
+  age: 15,
+  friends: [
+    { name: "tom", age: 15 },
+    { name: "dora", age: 15 },
+  ],
+};
+
+let obj2 = { ...obj };
+obj2.friends[0].name = "CHANGED NAME";
+
+// name меняется у обоих объектов
+console.log(obj.friends[0].name); // CHANGED NAME
+console.log(obj2.friends[0].name); // CHANGED NAME
