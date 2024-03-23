0715d46d89c1ae00f5fe2d8332b3852b67ca1293
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-03-23 14:35:06 +0300

    dfdf

--- LearnJS_6/6.3 Область видимости, замыкание/index.js
+++ LearnJS_6/6.3 Область видимости, замыкание/index.js
@@ -9,3 +9,19 @@ function makeWorker() {
 if (true) {
   makeWorker()();
 }
+
+{
+  var name1 = "string";
+}
+
+console.log(name1);
+
+function sayHi() {
+  var foo = "hello";
+
+  console.log(foo);
+}
+
+sayHi();
+
+console.log(foo);
