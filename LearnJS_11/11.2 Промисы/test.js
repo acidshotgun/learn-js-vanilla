0c4aa91dc64e5f9ea0a31c2d92327420124ba70c
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-04-22 12:04:20 +0300

    df

--- LearnJS_11/11.2 Промисы/test.js
+++ LearnJS_11/11.2 Промисы/test.js
@@ -1,4 +1,9 @@
 const showMessage = (message) => {
+  /*
+    Ф-я возвращает промис
+    Внутри исполнитель (executor)
+    В зависимости от условия вызывает resolve|reject
+  */
   return new Promise((resolve, reject) => {
     if (message.length < 10) {
       setTimeout(() => {
@@ -12,7 +17,54 @@ const showMessage = (message) => {
   });
 };
 
+/*
+  Вызов showMessage() вернет промис, который нужно обработать
+  Используется then|catch (finally - опционально)
+*/
 showMessage("Привет! Теперь это длинное сообщение")
   .then((res) => console.log(res))
   .catch((error) => console.log(error))
   .finally(() => console.log("Промис завершен."));
+
+////
+////
+////
+///
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Выполнилось через ${ms / 1000} секунды`);
+    }, ms);
+  });
+}
+
+delay(5000).then((res) => console.log(res));
+
+///
+///
+//\
+
+new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(1), 1000);
+})
+  .then(function (result) {
+    console.log(result); // 1
+
+    return new Promise((resolve, reject) => {
+      // (*)
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  })
+  .then(function (result) {
+    // (**)
+
+    console.log(result); // 2
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  })
+  .then(function (result) {
+    console.log(result); // 4
+  });
