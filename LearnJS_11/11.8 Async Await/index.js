ef3fafbdf4538011e42c2ce4c89136cab63b93bd
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-04-30 17:08:08 +0300

    dfdf

--- LearnJS_11/11.8 Async Await/index.js
+++ LearnJS_11/11.8 Async Await/index.js
@@ -1,58 +1,20 @@
-// const fetchData = () => {
-//   return Promise.resolve({
-//     data: [
-//       { name: "Sasha", age: 15 },
-//       { name: "Tiffany", age: 16 },
-//       { name: "Bob", age: 14 },
-//       { name: "Jessica", age: 15 },
-//     ],
-//   });
-// };
-
-// Все ок
-// const fetchData = () => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve({
-//         data: [
-//           { name: "Sasha", age: 15 },
-//           { name: "Tiffany", age: 16 },
-//           { name: "Bob", age: 14 },
-//           { name: "Jessica", age: 15 },
-//         ],
-//       });
-//     }, 2000);
-//   });
-// };
-
-// Типа ошибка
-const fetchData = () => {
-  return new Promise((resolve, reject) => {
+const changeRegister = (str) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      reject("ОШИБКА");
-    }, 2000);
+      resolve(str.toUpperCase());
+    }, 1500);
   });
 };
 
-const getUsersData = () => {
-  fetchData()
-    .then((data) => {
-      console.log(data.data);
-      return "done";
-    })
-    .catch((data) => console.log(data));
+//
+changeRegister("hello world").then((res) => console.log(res));
+
+//
+
+const fetchToChangeRegister = async (str) => {
+  const result = await changeRegister(str);
+
+  console.log(result);
 };
 
-const getUsersData2 = async () => {
-  try {
-    let result = await fetchData();
-    console.log(result.data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getUsersData();
-getUsersData2();
-
-console.log("старт");
+fetchToChangeRegister("hello world");
